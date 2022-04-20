23196b580e36f472c7aa5be3d824ef780add7ffc
jomazi <>
2022-04-20 11:30:46 +0200

    setup python

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import argparse
+
+if __name__ == "__main__":
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--twitter", help="collect Twitter data", action="store_true")
+    parser.add_argument("--euro2020", help="get EURO2020 information", action="store_true")
+
+    args = parser.parse_args()
+
+    # run task requested by user
+    if args.twitter:
+        print("Twitter")
+    elif args.euro2020:
+        print("EURO2020")

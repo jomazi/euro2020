8c6528446c46408c4d56c0599f69207658979045
jomazi <>
2022-04-21 11:43:13 +0200

    add extraction of network data

--- src/twitter/collect.py
+++ src/twitter/collect.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import os
 
 from twarc import Twarc2, expansions
@@ -24,4 +25,51 @@ def search_tweets():
     for page in search_results:
         result = expansions.flatten(page)
         for tweet in result:
-            print("Tweet: ", tweet)
+            print(f"Tweet: {json.dumps(tweet, indent=4)}")
+
+            # timestamp (UNIX time)
+            created_at = tweet["created_at"]
+            timestamp = int(datetime.datetime.strptime(
+                created_at, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=datetime.timezone.utc).timestamp())  # second resolution is enough
+            print(f"Tweet date: {created_at}")
+            print(f"Tweet timestamp: {timestamp}")
+
+            # hashtags
+            hashtags = []
+            try:
+                hashtags = tweet["entities"]["hashtags"]
+                hashtags = list(set([h["tag"] for h in hashtags]))    # remove duplicates
+            except KeyError:
+                pass
+
+            # mentions
+            mentions = []
+            try:
+                mentions = tweet["entities"]["mentions"]
+                mentions = list(set([m["username"] for m in mentions]))    # remove duplicates
+            except KeyError:
+                pass
+
+            # tweet author
+            author = tweet["author"]["username"]
+
+            print("Hashtags: ", hashtags)
+            print("Author: ", author)
+            print("Mentions: ", mentions)
+
+            # edges
+            user_mentions = [(author, m) for m in mentions]
+            user_hashtag = [(author, h) for h in hashtags]
+
+            hashtag_hashtag = []    # hashtag co-occurrence
+            for i, h1 in enumerate(hashtags):
+                hashtag_hashtag.extend([(h1, h2) for h2 in hashtags[i + 1:]])
+
+            user_user = []    # user co-occurrence
+            for i, u1 in enumerate(mentions):
+                user_user.extend([(u1, u2) for u2 in mentions[i + 1:]])
+
+            print("Edges user mentions: ", user_mentions)
+            print("Edges user hashtag: ", user_hashtag)
+            print("Edges hashtag co-occurrence: ", hashtag_hashtag)
+            print("Edges user co-occurrence: ", user_user)
